208e71f5179c124916d07be6fdf1d6c9a8998b81
yalya96 <yalya96@gmail.com>
2019-07-31 09:54:04 +0000

    systeme1

--- src/Controller/SystemeController.php
+++ src/Controller/SystemeController.php
@@ -4,6 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SystemeController extends AbstractController
 {
@@ -15,5 +20,46 @@ class SystemeController extends AbstractController
         return $this->render('systeme/index.html.twig', [
             'controller_name' => 'SystemeController',
         ]);
+    }
+    
+    /**
+     * @Route("/sys")
+     */
+    public function addsys(Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $entityManager)
+    {
+        $values = json_decode($request->getContent());
+        if(isset($values->username,$values->password,$values->Prenom,$values->Nom,$values->Telephone,$values->profil)) {
+            $user = new User();
+            $user->setUsername($values->username);
+            $user->setPassword($passwordEncoder->encodePassword($user, $values->password));
+            $user->setPrenom($values->Prenom);
+            $user->setNom($values->Nom);
+            $user->setTelephone($values->Telephone);
+            $user->setStatut("ACTIF");
+            if ($values->profil==1) {
+                $user->setRoles(['ROLE_ADMIN']);
+            }
+            if ($values->profil==2) {
+                $user->setRoles(['ROLE_CAISSIER']);
+            }
+            if ($values->profil==3) {
+                $user->setRoles(['ROLE_PRESTATAIRE']);
+            }
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $data = [
+                'status' => 201,
+                'message' => 'L\'utilisateur a été créé'
+            ];
+
+            return new JsonResponse($data, 201);
+        }
+        $data = [
+            'status' => 500,
+            'message' => 'Vous devez renseigner les clés username et password'
+        ];
+        return new JsonResponse($data, 500);
+
     }
 }
